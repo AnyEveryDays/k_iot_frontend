4e82f67f4ace99caa05b682c4cddaca2e127eb6b
AnyEveryDays <stk9460@naver.com>
2025-10-29 14:35:04 +0900

    리액트 설정

---
+++ f-iot-react/src/react_vite.ts
@@ -0,0 +1,14 @@
+// react_vite.ts
+
+//! vite를 사용한 리액트 프로젝트 생성
+// 이상하면 npm i or vsc 껏다가 다시 실행 (폴더 경로 제대로 확인 리액트 폴더에서 터미널 열어야함)
+
+//? cf) npm 7+ (버전 확인 필요)
+// node -v
+// npm -v
+
+//? 리액트 프로젝트 생성
+// npm create vite@latest(프로젝트명첨부)
+
+//! 프로젝트 설정 순서 (터미널에서 대화형으로 진행)
+// Project name:
---
+++ f-iot-react/src/routes/index.ts
@@ -0,0 +1,6 @@
+export const tmp = '';
+
+//! routes 폴더
+// : React Router 설정
+// - AppRouter.tsx
+// - ProtectedRoute.tsx
